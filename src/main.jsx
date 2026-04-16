9761742be2bf4b63406565e8ed96113318024ece
maheerashakeel <mahira16124@gmail.com>
2026-04-16 17:21:59 +0500

    fix github pages router issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <StoreContextProvider>
-  <App />
-    </StoreContextProvider>
-    
-    </BrowserRouter>
+    <HashRouter>
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </HashRouter>
   </StrictMode>
 )
